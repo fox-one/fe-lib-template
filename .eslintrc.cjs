5eee7cf6bba58339be279d1e9c63aff819265a63
zhangqingwu <18656007202@163.com>
2021-11-12 18:27:16 +0800

    feat: 🎸 eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,5 +14,16 @@ module.exports = {
   parserOptions: {
     ...base.parserOptions,
     project: ["./tsconfig.json"]
+  },
+  rules: {
+    ...base.rules,
+    "sort-keys": "off",
+    "vue/component-definition-name-casing": "off",
+    "vue/require-default-prop": "off",
+    "@typescript-eslint/no-floating-promises": "off",
+    "@typescript-eslint/unbound-method": "off",
+    "@typescript-eslint/ban-types": "off",
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/restrict-plus-operands": "off"
   }
 };
